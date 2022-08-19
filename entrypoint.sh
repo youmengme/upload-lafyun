2c9f5cecf3bb17452d9204c92a4892ffd338d70a
youmeng <1410180317@qq.com>
2022-08-19 21:46:10 +0800

    feat: update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,9 +11,7 @@ echo "npm version: $(npm -v)"
 cd $GITHUB_WORKSPACE
 
 # 安装 laf-cli 命令行工具
-npm install -g laf-cli
-
-laf-cli sync \
+npm install -g laf-cli && laf-cli sync \
   --access-key ${ACCESS_KEY} \
   --access-secret ${ACCESS_SECRET} \
   --bucket-name ${BUCKET_NAMEet} \
